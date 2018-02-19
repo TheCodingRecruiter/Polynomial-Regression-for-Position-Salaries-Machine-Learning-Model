7a3aa5920d5e8adac29cc54f87e811fd4876b5d3
Wes Eby <richard.eby@hotmail.com>
2018-02-19 11:17:36 -0500

    Initial Commit
    Initial Commit

---
+++ Polynomial Regression for Position Salaries Machine Learning Model.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 19 09:22:30 2018
+
+@author: richa
+"""
+
+# Importing the Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv(r'C:\Users\richa\MachineLearningProjects\Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values # Matrix of Features
+Y = dataset.iloc[:, 2].values # Dependent Variable/Target Values
+
+# Fitting Linear Regression into the Dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X,Y)
+
+# Fitting Polynomial Regression into the Dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, Y)
+
+# Visualizing the Linear Regression results
+plt.scatter(X, Y, color = 'red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Linear Regression Graph')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+
+# Visualizing the Polynomial Regression results
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, Y, color = 'red')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.title('Polynomial Regression Graph')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+# Predicting a new result with Linear Regression
+lin_reg.predict(5)
+
+# Predicting a new result with Polynomial Regression
+lin_reg_2.predict(poly_reg.fit_transform(5))
